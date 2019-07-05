cb39a270c9bf552d2cb8ade704b2d8c5139ee90d
Justin Davis <justinndavis@gmail.com>
2019-07-05 22:06:01 +0100

    cicd_tools update

--- cicd_pipelines/angular.pipeline.groovy
+++ cicd_pipelines/angular.pipeline.groovy
@@ -8,8 +8,7 @@ node('nodejs') {
 
     dir("src/${app_name}") {
 
-      def dev_project  = "${org}-dev"
-      def prod_project = "${org}-prod"
+      def project  = "${org}"
       def sonar_url    = "http://sonarqube.cicd.svc:9000"
 
       stage('npm') {
@@ -25,25 +24,25 @@ node('nodejs') {
       //Build the OpenShift Image in OpenShift and tag it.
       stage('Dev : Build and Tag OpenShift Image') {
         echo "Building OpenShift container image ${app_name}:${devTag}"
-        echo "Project : ${dev_project}"
+        echo "Project : ${project}"
         echo "App : ${app_name}"
 
-        sh "oc start-build ${app_name} --follow --from-dir=dist/Angular7 -n ${dev_project}"
-        openshiftVerifyBuild apiURL: '', authToken: '', bldCfg: app_name, checkForTriggeredDeployments: 'false', namespace: dev_project, verbose: 'false', waitTime: ''
-        openshiftTag alias: 'false', apiURL: '', authToken: '', destStream: app_name, destTag: devTag, destinationAuthToken: '', destinationNamespace: dev_project, namespace: dev_project, srcStream: app_name, srcTag: 'latest', verbose: 'false'
+        sh "oc start-build ${app_name} --follow --from-dir=dist/Angular7 -n ${project}"
+        openshiftVerifyBuild apiURL: '', authToken: '', bldCfg: app_name, checkForTriggeredDeployments: 'false', namespace: project, verbose: 'false', waitTime: ''
+        openshiftTag alias: 'false', apiURL: '', authToken: '', destStream: app_name, destTag: devTag, destinationAuthToken: '', destinationNamespace: project, namespace: project, srcStream: app_name, srcTag: 'latest', verbose: 'false'
       }
 
       // Deploy the built image to the Development Environment.
       stage('Dev :  Deploy') {
         echo "Deploying container image to Development Project"
-        echo "Project : ${dev_project}"
+        echo "Project : ${project}"
         echo "App : ${app_name}"
         echo "Dev Tag : ${devTag}"
 
-        sh "oc set image dc/${app_name} ${app_name}=docker-registry.default.svc:5000/${dev_project}/${app_name}:${devTag} -n ${dev_project}"
+        sh "oc set image dc/${app_name} ${app_name}=docker-registry.default.svc:5000/${project}/${app_name}:${devTag} -n ${project}"
 
-        openshiftDeploy apiURL: '', authToken: '', depCfg: app_name, namespace: dev_project, verbose: 'false', waitTime: '180', waitUnit: 'sec'
-        openshiftVerifyDeployment apiURL: '', authToken: '', depCfg: app_name, namespace: dev_project, replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '180', waitUnit: 'sec'
+        openshiftDeploy apiURL: '', authToken: '', depCfg: app_name, namespace: project, verbose: 'false', waitTime: '180', waitUnit: 'sec'
+        openshiftVerifyDeployment apiURL: '', authToken: '', depCfg: app_name, namespace: project, replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '180', waitUnit: 'sec'
       }
     }
 }
