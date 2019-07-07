0f0afbdc2ef5046694d0fcb09b20294489deba54
Justin Davis <justinndavis@gmail.com>
2019-07-07 12:49:32 +0100

    cicd_tools update

--- env.sh
+++ env.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-export IP=192.168.0.22.xip.io
+export IP=192.168.0.2.xip.io
 export USER=justin
 
 export CICD_PROJECT=cicd
 export PROJECT=amazin
 
 export CURL="curl -k -v"
-export JENKINS=jenkins-cicd.apps.192.168.0.22.xip.io
+export JENKINS=jenkins-cicd.apps.192.168.0.2.xip.io
 export JENKINS_TOKEN=119d9988d1ef7a30bea612805759277441
 export JENKINS_USER=justin-admin-edit-view
 
